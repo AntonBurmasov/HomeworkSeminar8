f99552724e51d884a1f3fd7cc771c1014e6232a0
burmasovanton <looseangel1@gmail.com>
2022-09-27 17:58:45 +0500

    working task54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -58,29 +58,32 @@ void OrderedArray(int[,] matrix)
 {   
     
     int temp = 0;
-    int b = 0;
+    for(int q = 0; q < matrix.GetLength(1); q++)
+    {
      for(int i = 0; i < matrix.GetLength(0); i++)
       {  int a = i;
-         b = 0; 
-        for(int j = 0; j < matrix.GetLength(1); j++)
+         int b = 0; 
+        for(int j = 1; j < matrix.GetLength(1); j++)
         {
             
-            if(matrix[i,j]>matrix[a,b])
+            if(matrix[i,j]<matrix[a,b])
             {
               temp = matrix[a,b];
               matrix[a,b] = matrix[i,j];
               matrix[i,j] = temp;
-              b++; 
-            }
               
+               
+            }
+           b++;   
         }
         
-           
+        b = 1;
+        a++;   
         } 
           
         
       }
-
+}
 
 
 
@@ -89,6 +92,7 @@ int n = ReadNumber("Введите количество столбцов");
 int[,] matr = GetMatrix(m, n);
 PrintMatrix(matr);
 OrderedArray(matr);
+Console.WriteLine("");
 PrintMatrix(matr);
 
 

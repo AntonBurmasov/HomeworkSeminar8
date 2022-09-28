165ae218c988333178eb6e2628d25889affccc78
burmasovanton <looseangel1@gmail.com>
2022-09-28 18:25:48 +0500

    not working task

---
+++ Task62/Program.cs
@@ -0,0 +1,103 @@
+﻿// Задача 62. Заполните спирально массив 4 на 4.
+
+// Например, на выходе получается вот такой массив:
+
+// 1 2 3 4
+
+// 12 13 14 5
+
+// 11 16 15 6
+
+// 10 9 8 7
+
+// Делал данную задачу сам, реализация топорная, но ничего лучше не придумал.
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}       ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+ int[,] matrix = new int[4, 4];
+
+ int i = 0;
+ int j = 0;
+ int value = 1;
+
+
+    while(j< matrix.GetLength(0))
+    {
+     matrix[i,j] = value;
+      value++;
+      j++;
+    }
+     i = i + 1;
+     j = j - 1;
+
+    while(i< matrix.GetLength(1))
+    {
+       matrix[i, j] = value;
+      value++;
+      i++;
+
+    }
+  j = j - 1;
+  i = i - 1;
+  
+
+   while(j > -1)
+    { 
+       matrix[i ,j] = value;
+      value++;
+      j = j - 1;
+     
+    }
+
+ j = j + 1;
+i = i - 1;
+
+ while(i > 0)
+ {
+  matrix[i,j] = value;
+  value++;
+  i = i - 1;
+
+}
+i = i + 1;
+j = j + 1;
+ while(j < 3)
+ {
+  matrix[i,j] = value;
+  value++;
+  j++;
+ }
+ j = j - 1;
+i = i - 1;
+
+ while(i < 3)
+ {
+  matrix[i,j]= value;
+  value++;
+ i++;
+ }
+ i = i - 1;
+ j = j - 1;
+
+ while(j > 0)
+ {
+  matrix[i,j] = value;
+  value++;
+  j--;
+
+ }
+ 
+
+PrintMatrix(matrix);
